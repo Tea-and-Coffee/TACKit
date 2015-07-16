47a68ef727e2f9cf02c44072618377f9d1305c41
companion_ios <companion_ios@Satoru-no-iMac.local>
2015-07-16 16:22:30 +0900

    TACKit  - PlaceHolderTextView: Refactoring

--- TACKit/UITextView/PlaceHolderTextView.h
+++ TACKit/UITextView/PlaceHolderTextView.h
@@ -1,6 +1,6 @@
 //
 //  PlaceHolderTextView.h
-//  MobileMessenger
+//  TACKitExample
 //
 //  Created by masato_arai on 2015/04/27.
 //  Copyright (c) 2015年 Tea and Coffee. All rights reserved.
@@ -9,6 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface PlaceHolderTextView : UITextView
+
+@property (nonatomic, strong, readonly) UILabel *placeHolderLabel;
 
 @property (nonatomic, strong) NSString *placeholder;
 @property (nonatomic, strong) UIColor *placeholderColor;
--- TACKit/UITextView/PlaceHolderTextView.m
+++ TACKit/UITextView/PlaceHolderTextView.m
@@ -10,8 +10,6 @@
 
 @interface PlaceHolderTextView ()
 
-@property (nonatomic, strong) UILabel *placeHolderLabel;
-
 @end
 
 @implementation PlaceHolderTextView
@@ -25,35 +23,34 @@
     
     // Use Interface Builder User Defined Runtime Attributes to set
     // placeholder and placeholderColor in Interface Builder.
-    if (!self.placeholder) {
-        [self setPlaceholder:@""];
+    if (!_placeholder) {
+        _placeholder = @"";
     }
     
-    if (!self.placeholderColor) {
-        [self setPlaceholderColor:[UIColor lightGrayColor]];
+    if (!_placeholderColor) {
+        _placeholderColor = [UIColor lightGrayColor];
     }
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textChanged:) name:UITextViewTextDidChangeNotification object:nil];
 }
 
 - (id)initWithFrame:(CGRect)frame {
-    if( (self = [super initWithFrame:frame]) ) {
-        [self setPlaceholder:@""];
-        [self setPlaceholderColor:[UIColor lightGrayColor]];
+    if ((self = [super initWithFrame:frame])) {
+        _placeholder = @"";
+        _placeholderColor = [UIColor lightGrayColor];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textChanged:) name:UITextViewTextDidChangeNotification object:nil];
     }
     return self;
 }
 
 - (void)textChanged:(NSNotification *)notification {
-    if([[self placeholder] length] == 0) {
+    if ([_placeholder length] == 0) {
         return;
     }
     
-    if([[self text] length] == 0) {
+    if ([[self text] length] == 0) {
         [[self viewWithTag:999] setAlpha:1];
-    }
-    else {
+    } else {
         [[self viewWithTag:999] setAlpha:0];
     }
 }
@@ -64,25 +61,25 @@
 }
 
 - (void)drawRect:(CGRect)rect {
-    if( [[self placeholder] length] > 0 ) {
-        if (_placeHolderLabel == nil ) {
+    if ([_placeholder length] > 0) {
+        if (_placeHolderLabel == nil) {
             _placeHolderLabel = [[UILabel alloc] initWithFrame:CGRectMake(5,8,self.bounds.size.width - 16,0)];
             _placeHolderLabel.lineBreakMode = NSLineBreakByWordWrapping;
             _placeHolderLabel.numberOfLines = 0;
             _placeHolderLabel.font = self.font;
             _placeHolderLabel.backgroundColor = [UIColor clearColor];
-            _placeHolderLabel.textColor = self.placeholderColor;
+            _placeHolderLabel.textColor = _placeholderColor;
             _placeHolderLabel.alpha = 0;
             _placeHolderLabel.tag = 999;
             [self addSubview:_placeHolderLabel];
         }
         
-        _placeHolderLabel.text = self.placeholder;
+        _placeHolderLabel.text = _placeholder;
         [_placeHolderLabel sizeToFit];
         [self sendSubviewToBack:_placeHolderLabel];
     }
     
-    if( [[self text] length] == 0 && [[self placeholder] length] > 0 ) {
+    if ([[self text] length] == 0 && [_placeholder length] > 0) {
         [[self viewWithTag:999] setAlpha:1];
     }
     

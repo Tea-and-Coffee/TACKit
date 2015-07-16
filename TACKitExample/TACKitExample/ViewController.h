5a02df50f62d974a3f947903b4b8d9905c648cb1
companion_ios <companion_ios@Satoru-no-iMac.local>
2015-07-16 14:37:18 +0900

    Create TACKitExample.

---
+++ TACKitExample/TACKitExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TACKitExample
+//
+//  Created by companion_ios on 20150716.
+//  Copyright (c) 2015年 masato_arai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TACKitExample/TACKitExampleTests/TACKitExampleTests.m
@@ -0,0 +1,40 @@
+//
+//  TACKitExampleTests.m
+//  TACKitExampleTests
+//
+//  Created by companion_ios on 20150716.
+//  Copyright (c) 2015年 masato_arai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TACKitExampleTests : XCTestCase
+
+@end
+
+@implementation TACKitExampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

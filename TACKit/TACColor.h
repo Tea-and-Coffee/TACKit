b901f8e18a7f2fe2d143ae9145d4facc01ec2203
m-arai <tf-arai@tf-admin20150731-no-macbook-pro.local>
2015-11-27 19:05:26 +0900

    TACKit  - TACColor

--- TACKit/TACColor.h
+++ TACKit/TACColor.h
@@ -6,22 +6,25 @@
 //  Copyright (c) 2015年 Tea and Coffee. All rights reserved.
 //
 
+#define RGB(r, g, b)            [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1.0]
+#define RGBA(r, g, b, a)        [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:a]
+
 // iOS 7 Colors
-#define REDCOLOR_FFFF3B30       [UIColor colorWithRed:255/255.0 green:59/255.0 blue:48/255.0 alpha:1.0]
-#define ORANGECOLOR_FFFF9500    [UIColor colorWithRed:255/255.0 green:149/255.0 blue:0/255.0 alpha:1.0]
-#define YELLOWCOLOR_FFFFCC00    [UIColor colorWithRed:255/255.0 green:204/255.0 blue:0/255.0 alpha:1.0]
-#define GREENCOLOR_FF4CD964     [UIColor colorWithRed:76/255.0 green:217/255.0 blue:100/255.0 alpha:1.0]
-#define TEALCOLOR_FF34AADC      [UIColor colorWithRed:52/255.0 green:170/255.0 blue:220/255.0 alpha:1.0]
-#define BLUECOLOR_FF007AFF      [UIColor colorWithRed:0/255.0 green:122/255.0 blue:255/255.0 alpha:1.0]     // System TintColor
-#define VIOLETCOLOR_FF5856D6    [UIColor colorWithRed:88/255.0 green:86/255.0 blue:214/255.0 alpha:1.0]
-#define PINKCOLOR_FFFF2D55      [UIColor colorWithRed:255/255.0 green:45/255.0 blue:85/255.0 alpha:1.0]
+#define REDCOLOR_FFFF3B30       [UIColor colorWithRed:255/255.0 green: 59/255.0 blue: 48/255.0 alpha:1.0]
+#define ORANGECOLOR_FFFF9500    [UIColor colorWithRed:255/255.0 green:149/255.0 blue:  0/255.0 alpha:1.0]
+#define YELLOWCOLOR_FFFFCC00    [UIColor colorWithRed:255/255.0 green:204/255.0 blue:  0/255.0 alpha:1.0]
+#define GREENCOLOR_FF4CD964     [UIColor colorWithRed: 76/255.0 green:217/255.0 blue:100/255.0 alpha:1.0]
+#define TEALCOLOR_FF34AADC      [UIColor colorWithRed: 52/255.0 green:170/255.0 blue:220/255.0 alpha:1.0]
+#define BLUECOLOR_FF007AFF      [UIColor colorWithRed:  0/255.0 green:122/255.0 blue:255/255.0 alpha:1.0]   // System TintColor
+#define VIOLETCOLOR_FF5856D6    [UIColor colorWithRed: 88/255.0 green: 86/255.0 blue:214/255.0 alpha:1.0]
+#define PINKCOLOR_FFFF2D55      [UIColor colorWithRed:255/255.0 green: 45/255.0 blue: 85/255.0 alpha:1.0]
 #define MIDGRAYCOLOR_FF8E8E93   [UIColor colorWithRed:142/255.0 green:142/255.0 blue:147/255.0 alpha:1.0]
 #define GRAYCOLOR_FFC7C7CC      [UIColor colorWithRed:199/255.0 green:199/255.0 blue:204/255.0 alpha:1.0]
 
 // This should verification.
-#define GRAYCOLOR_FF58595A      [UIColor colorWithRed:88/255.0 green:89/255.0 blue:90/255.0 alpha:1.0]      // UINavigationBar Title Color
-#define GRAYCOLOR_FFF8F8F8      [UIColor colorWithWhite:248/255.0  alpha:1.000]                             // UINavigationBar Background Color
-#define GRAYCOLOR_FFFAFAFA      [UIColor colorWithWhite:250/255.0 alpha:1.000]                              // UITabbar, UIToolbar and TableView HeaderView Background Color
-#define GRAYCOLOR_FFB2B2B2      [UIColor colorWithWhite:178/255.0 alpha:1.000]                              // Bar Separator Color
-#define GRAYCOLOR_FFCDCDCD      [UIColor colorWithWhite:205/255.0 alpha:1.000]                              // UITextField Separator Color
-#define GRAYCOLOR_FF929292      [UIColor colorWithWhite:146/255.0 alpha:1.000]                              // UITabBarItem TintColor(Gray)
+#define GRAYCOLOR_FF58595A      [UIColor colorWithRed: 88/255.0  green: 89/255.0 blue:90/255.0 alpha:1.0]   // UINavigationBar Title Color
+#define GRAYCOLOR_FFF8F8F8      [UIColor colorWithWhite:248/255.0 alpha:1.0]                                // UINavigationBar Background Color
+#define GRAYCOLOR_FFFAFAFA      [UIColor colorWithWhite:250/255.0 alpha:1.0]                                // UITabbar, UIToolbar and TableView HeaderView Background Color
+#define GRAYCOLOR_FFB2B2B2      [UIColor colorWithWhite:178/255.0 alpha:1.0]                                // Bar Separator Color
+#define GRAYCOLOR_FFCDCDCD      [UIColor colorWithWhite:205/255.0 alpha:1.0]                                // UITextField Separator Color
+#define GRAYCOLOR_FF929292      [UIColor colorWithWhite:146/255.0 alpha:1.0]                                // UITabBarItem TintColor(Gray)
